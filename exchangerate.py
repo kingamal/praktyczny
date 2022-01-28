f07c8ec5cc3ad5755af9d37395a7166c67f58813
Kinga <kinga.maleszewska@o2.pl>
2022-01-28 18:51:34 +0100

    Exchange rate generator using api

---
+++ exchangerate.py
@@ -0,0 +1,20 @@
+from requests import get
+
+code = "EUR" #other params ["USD", "EUR", "CHF"]
+date = "2021-12-15" #change the date
+
+url = f'http://api.nbp.pl/api/exchangerates/rates/a/{code}/{date}/'
+
+resp = get(url)
+data = resp.json()
+currency = data['rates'][0]['mid']
+
+print(currency)
+
+exchange_rate = "1 {} = {} PLN w dniu {}".format(
+                                            data['code'],
+                                            currency,
+                                            data['rates'][0]['effectiveDate']
+                                            )
+
+print(exchange_rate)
