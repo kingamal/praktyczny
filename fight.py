02cfaccd2174280319e629c3e0525cfcc688d02a
Kinga <kinga.maleszewska@o2.pl>
2022-02-01 19:22:12 +0100

    Adding two more limitations

--- fight.py
+++ fight.py
@@ -2,13 +2,17 @@ password = input("Podaj nowe hasło: ")
 
 has_lower = False
 has_upper = False
+has_digit = False
 for char in password:  # idź znak po znaku
     if char.isalpha() and char.isupper():
         has_upper = True
     if char.isalpha() and char.islower():
         has_lower = True
+    if char.isdigit():
+        has_digit = True
+long_enough = len(password) >= 8
 
-is_correct = has_lower and has_upper
+is_correct = has_lower and has_upper and has_digit and long_enough
 if is_correct:
     print("Twoje hasło jest wystarczająco złożone")
 else:
@@ -17,3 +21,7 @@ else:
         print("- przynajmniej jedna mała litera")
     if not has_upper:
         print("- przynajmniej jedna duża litera")
+    if not has_digit:
+        print("- przynajmniej jedna cyfra")
+    if not long_enough:
+        print("- przynajmniej 8 znaków")
